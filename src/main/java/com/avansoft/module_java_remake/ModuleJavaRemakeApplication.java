c7f0aa62844937d0034b065d2b7556a36fcdf4e4
NGUYEN_DUY_2003\nguye <duy@gmail.com>
2024-11-08 13:27:32 +0700

    Initial commit

---
+++ src/main/java/com/avansoft/module_java_remake/ModuleJavaRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.avansoft.module_java_remake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleJavaRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleJavaRemakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avansoft/module_java_remake/ModuleJavaRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avansoft.module_java_remake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleJavaRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
