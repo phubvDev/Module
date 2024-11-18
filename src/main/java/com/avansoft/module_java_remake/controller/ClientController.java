17e515a18e40343d8f3626d1f9f528baa92c6cc3
NGUYEN_DUY_2003\nguye <duy@gmail.com>
2024-11-18 09:04:50 +0700

    update

--- src/main/java/com/avansoft/module_java_remake/controller/ClientController.java
+++ src/main/java/com/avansoft/module_java_remake/controller/ClientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/client")
+@RequestMapping("${API_BASE_URL}/client")
 public class ClientController {
     @Autowired
     private IClientService clientService;
