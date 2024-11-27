3f5de2215eca98604319209a6b3b2e1fc45f8bf4
buidangphu <buivanphu62thnb@gmail.com>
2024-11-27 11:38:36 +0700

    add proxy to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://13.124.14.236:8386', // URL của backend API
+        changeOrigin: true, // Đảm bảo Origin của request chính xác
+        rewrite: (path) => path.replace(/^\/api/, ''), // Loại bỏ phần prefix /api khi gửi yêu cầu
+      },
+    },
+  },
 })
