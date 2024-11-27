daf39e9cdfd532063f65ef15d4564372aec9d2cd
buidangphu <buivanphu62thnb@gmail.com>
2024-11-27 10:21:10 +0700

    change origin url

--- src/main/java/com/avansoft/module_java_remake/config/WebConfig.java
+++ src/main/java/com/avansoft/module_java_remake/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://13.124.14.236"); // Cho phép origin của frontend
+        config.addAllowedOrigin("http://localhost:5173/"); // Cho phép origin của frontend
         config.addAllowedMethod("*"); // Cho phép tất cả các method (GET, POST, etc.)
         config.addAllowedHeader("*"); // Cho phép tất cả các header
         config.setAllowCredentials(true); // Cho phép gửi cookie
