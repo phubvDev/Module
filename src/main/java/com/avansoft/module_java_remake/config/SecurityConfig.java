cc6e4478d34406e3f206c8e1245ace478157725d
NGUYEN_DUY_2003\nguye <duy@gmail.com>
2024-11-27 09:10:12 +0700

    update register

--- src/main/java/com/avansoft/module_java_remake/config/SecurityConfig.java
+++ src/main/java/com/avansoft/module_java_remake/config/SecurityConfig.java
@@ -18,27 +18,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
-                .csrf().disable()
-                .cors().and()
-                .authorizeRequests()
-                .antMatchers("/auth/login","/api/avansoft/module/auth/login").permitAll()
-                .anyRequest().authenticated()
+                .cors() // Chỉ cần gọi một lần
                 .and()
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                        .sessionManagement()
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/api/avansoft/module/auth/login","/api/avansoft/module/users/register","/api/avansoft/module/email/create","/api/avansoft/module/users/check-username").permitAll() // Cho phép các endpoint login không cần xác thực
+                .anyRequest().authenticated() // Tất cả các request khác đều cần xác thực
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class) // Thêm filter xác thực JWT
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // Chế độ Stateless để không sử dụng session
         System.out.println("Security Configuration applied successfully");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // Cung cấp PasswordEncoder để mã hóa mật khẩu
     }
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter();
+        return new JwtAuthenticationFilter(); // Cung cấp JwtAuthenticationFilter để kiểm tra token
     }
-
 }
--- src/main/java/com/avansoft/module_java_remake/controller/AuthController.java
+++ src/main/java/com/avansoft/module_java_remake/controller/AuthController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("${API_BASE_URL}/auth")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
     private final IUserService userService;
     public AuthController(@Qualifier("userServiceImpl") IUserService userService) {
