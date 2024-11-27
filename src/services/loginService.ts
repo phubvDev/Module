297d06819156e1582a9cb0d702d29487eb9bd1a4
buidangphu <buivanphu62thnb@gmail.com>
2024-11-27 10:09:56 +0700

    urrl

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -1,7 +1,7 @@
 import axiosInstance from './index';
 
 export const loginService = (userId: string, password: string) => {
-    return axiosInstance.post('/auth/login', {
+    return axiosInstance.post('http://13.124.14.236:8386/api/avansoft/module/auth/login', {
         userId,
         password,
     });
