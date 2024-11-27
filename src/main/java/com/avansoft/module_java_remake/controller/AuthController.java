73c95bd567684893b531397c6535d3caa3462c7d
buidangphu <buivanphu62thnb@gmail.com>
2024-11-27 10:23:36 +0700

    change origin url

--- src/main/java/com/avansoft/module_java_remake/controller/AuthController.java
+++ src/main/java/com/avansoft/module_java_remake/controller/AuthController.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("${API_BASE_URL}/auth")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AuthController {
     private final IUserService userService;
     public AuthController(@Qualifier("userServiceImpl") IUserService userService) {
